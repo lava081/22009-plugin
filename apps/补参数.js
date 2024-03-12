5050f61b87ca628444fb770bbdc444c780fc1861
岩浆 <linboyan5438081@163.com>
2024-03-12 12:27:13 +0800

    补充getMemberMap

--- apps/补参数.js
+++ apps/补参数.js
@@ -63,8 +63,47 @@ export class giveNickname extends plugin {
         this.e.sender.card = user.nickname
         this.e.user_id = user.qq
         this.e.author.id = user.qq
+        if (e.group_id) {
+          Bot[e.self_id].pickGroup(e.group_id).getMemberMap = async () => await this.getMemberMap(e.group_id)
+          e.group.getMemberMap = async () => await this.getMemberMap(e.group_id)
+        }
       }
     }
     return false
+  }
+
+  async getMemberMap (group_id) {
+    let group_Member = new Map()
+    let member_list
+    try {
+      const group = await Openid.Group.findByPk(group_id)
+      member_list = await group.getUsers({})
+    } catch (error) {
+      Promise.reject(new Error('22009获取成员失败'))
+    }
+    member_list.forEach(user => {
+      group_Member.set(user.qq, {
+        group_id,
+        user_id: user.qq,
+        user_openid: user.user_id,
+        nickname: user.nickname,
+        card: '',
+        sex: 'unknown',
+        age: 0,
+        area: '',
+        join_time: Math.floor(user.createdAt.getTime() / 1000),
+        last_sent_time: Math.floor(user.updatedAt.getTime() / 1000),
+        level: 1,
+        role: 'member',
+        unfriendly: true,
+        title: '',
+        title_expire_time: 0,
+        shutup_time: 0,
+        update_time: 0,
+        card_changeable: false,
+        uin: user.self_id
+      })
+    })
+    return group_Member
   }
 }
